683b199bc2020e7c499741dd08f137a30e523661
Jon Gilmour <jmgilmour88@gmail.com>
2017-07-23 08:41:14 -0400

    Moving config to their own package

--- spsos-server/src/main/java/com/umuc/cmsc495/spsos/config/BeanConfiguration.java
+++ spsos-server/src/main/java/com/umuc/cmsc495/spsos/config/BeanConfiguration.java
@@ -1,4 +1,4 @@
-package com.umuc.cmsc495.spsos;
+package com.umuc.cmsc495.spsos.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
--- spsos-server/src/main/java/com/umuc/cmsc495/spsos/config/SecurityConfiguration.java
+++ spsos-server/src/main/java/com/umuc/cmsc495/spsos/config/SecurityConfiguration.java
@@ -1,4 +1,4 @@
-package com.umuc.cmsc495.spsos;
+package com.umuc.cmsc495.spsos.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
